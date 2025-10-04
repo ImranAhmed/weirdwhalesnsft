114e499905ab9cbd60a463fae67466c34541c886
Imran Ahmed <imran@greengateventures.com>
2025-10-04 12:48:23 +0100

    Fixed image optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   sassOptions: {
     quietDeps: true,
     silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin', 'color-functions'],
